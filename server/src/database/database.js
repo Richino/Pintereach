593eba3b698011081cd33f3dd0feb4fab66090e0
Richino <archerrichino@gmail.com>
2021-08-20 23:13:54 -0400

    Update database.js

--- server/src/database/database.js
+++ server/src/database/database.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const URI = "mongodb+srv://Richie_2:Hesoyam1@pintereach.y9dzs.mongodb.net/Pintereach?retryWrites=true&w=majority";
+const URI = "";
 
 const database = () => {
 	mongoose.connect(URI, {
